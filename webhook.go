f524bf8758b4862388cb333366d36403a991368c
Abiola Ibrahim <abiola89@gmail.com>
2016-01-19 12:25:56 +0100

    Add webhook type config. Remove generic hook from defaults.

--- webhook.go
+++ webhook.go
@@ -12,19 +12,42 @@ type WebHook struct {
 	Next  middleware.Handler
 }
 
-// Interface for specific providers to implement.
+// HookConfig is a webhook handler configuration.
+type HookConfig struct {
+	Url    string // url to listen on for webhooks
+	Secret string // secret to validate hooks
+	Type   string // type of Webhook
+}
+
+// hookHandler is interface for specific providers to implement.
 type hookHandler interface {
 	DoesHandle(http.Header) bool
 	Handle(w http.ResponseWriter, r *http.Request, repo *Repo) (int, error)
 }
 
-// Slice of all registered hookHandlers.
-// Register new hook handlers here!
-var handlers = []hookHandler{
+// handlers stores all registered hookHandlers.
+// map key corresponds to expected config name.
+//
+// register hook handlers here.
+var handlers = map[string]hookHandler{
+	"github":    GithubHook{},
+	"gitlab":    GitlabHook{},
+	"bitbucket": BitbucketHook{},
+	"generic":   GenericHook{},
+}
+
+// defaultHandlers is the list of handlers to choose from
+// if handler type is not specified in config.
+var defaultHandlers = []hookHandler{
 	GithubHook{},
 	GitlabHook{},
 	BitbucketHook{},
-	GenericHook{},
+}
+
+// registerHandler registers hook handler.
+// call on init() to register hook handler.
+func registerHandler(name string, handler hookHandler) {
+	handlers[name] = handler
 }
 
 // ServeHTTP implements the middlware.Handler interface.
@@ -32,9 +55,15 @@ func (h WebHook) ServeHTTP(w http.ResponseWriter, r *http.Request) (int, error) 
 
 	for _, repo := range h.Repos {
 
-		if r.URL.Path == repo.HookUrl {
+		if r.URL.Path == repo.Hook.Url {
 
-			for _, handler := range handlers {
+			// if handler type is specified.
+			if handler, ok := handlers[repo.Hook.Type]; ok {
+				return handler.Handle(w, r, repo)
+			}
+
+			// auto detect handler
+			for _, handler := range defaultHandlers {
 				// if a handler indicates it does handle the request,
 				// we do not try other handlers. Only one handler ever
 				// handles a specific request.
